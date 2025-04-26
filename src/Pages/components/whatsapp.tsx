a51b418bd2a4696f1350e10b3d410348b849c29a
Pawacode Database <whakhydoh@gmail.com>
2025-04-26 16:54:50 +0100

    po

--- src/Pages/components/whatsapp.tsx
+++ src/Pages/components/whatsapp.tsx
@@ -4,7 +4,7 @@ import { FaWhatsapp } from "react-icons/fa";
 const WhatsAppIcon: React.FC = () => {
   return (
     <a
-      href="https://wa.me/<YOUR_PHONE_NUMBER>" // Replace <YOUR_PHONE_NUMBER> with your WhatsApp number (include country code, e.g., 1234567890)
+      href="https://wa.me/+2348104682609>" // Replace <YOUR_PHONE_NUMBER> with your WhatsApp number (include country code, e.g., 1234567890)
       target="_blank"
       rel="noopener noreferrer"
       className="fixed bottom-[420px] right-2 bg-[#000] text-white p-4 rounded-full shadow-lg hover:bg-green-600 transition duration-300 z-50"
