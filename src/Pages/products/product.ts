315f731208f07d6f7cca1db3d0d5ceec46e4e8fb
Pawacode Database <whakhydoh@gmail.com>
2025-05-06 12:33:04 +0100

    po

--- src/Pages/products/product.ts
+++ src/Pages/products/product.ts
@@ -3,7 +3,7 @@ export interface Product {
     name: string;
     category: string;
     subCategories: string[];
-    price: number;
+    price?: number;
     originalPrice?: number; // For sale price comparison
     isOnSale?: boolean;
     description: string;
